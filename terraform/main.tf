bf77c693238f2348a754932deb760f9db4ccc12d
khaipd18 <khaipd180405@gmail.com>
2026-04-09 14:34:06 +0000

    modify root .tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,12 @@ module "vpc" {
   vpc_subnet_az_id = var.subnet_az_ids
   vpc_cidr         = var.vpc_cidr
 }
+
+module "ecr" {
+  source                = "./modules/ecr"
+  scan_on_push          = var.scan_on_push
+  image_tag_mutability  = var.image_tag_mutability
+  repository_name       = var.repository_name
+  allow_push_principals = var.allow_push_principals
+  allow_pull_principals = var.allow_pull_principals
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,6 @@
+# Variables for Terraform configuration
+
+#vpc configuration
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
 }
@@ -5,4 +8,30 @@ variable "vpc_cidr" {
 variable "subnet_az_ids" {
   type        = list(string)
   description = "List of availability zone IDs for subnets"
+}
+
+#ecr configuration
+variable "scan_on_push" {
+  description = "Whether to enable image scanning on push"
+  type        = bool
+}
+
+variable "repository_name" {
+  description = "The name of the ECR repository"
+  type        = string
+}
+
+variable "image_tag_mutability" {
+  description = "The tag mutability setting for the repository"
+  type        = string
+}
+
+variable "allow_push_principals" {
+  description = "List of principals allowed to push images to the repository"
+  type        = list(string)
+}
+
+variable "allow_pull_principals" {
+  description = "List of principals allowed to pull images from the repository"
+  type        = list(string)
 }
