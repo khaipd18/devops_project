7d27202efa42b265a7b93ff9c91efedc5cdfd42c
khaipd18 <khaipd180405@gmail.com>
2026-04-09 14:29:27 +0000

    add ecr module v1

---
+++ terraform/modules/ecr/main.tf
@@ -0,0 +1,74 @@
+resource "aws_ecr_repository" "ecr_repository" {
+  name                 = var.repository_name
+  image_tag_mutability = var.image_tag_mutability
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
+}
+
+resource "aws_ecr_repository_policy" "this" {
+  count      = (length(var.allow_push_principals) + length(var.allow_pull_principals)) > 0 ? 1 : 0
+  repository = aws_ecr_repository.ecr_repository.name
+  policy     = data.aws_iam_policy_document.repository_policy.json
+}
+
+resource "aws_ecr_lifecycle_policy" "untagged_images_policy" {
+  repository = aws_ecr_repository.ecr_repository.name
+  policy     = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Expire images older than 14 days",
+      "selection": {
+        "tagStatus": "untagged",
+        "countType": "sinceImagePushed",
+        "countUnit": "days",
+        "countNumber": 14
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_ecr_lifecycle_policy" "archived_images_policy" {
+  repository = aws_ecr_repository.ecr_repository.name
+  policy     = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Archive images not pulled in 90 days",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "sinceImagePulled",
+        "countUnit": "days",
+        "countNumber": 90
+      },
+      "action": {
+        "type": "transition",
+        "targetStorageClass": "archive"
+      }
+    },
+    {
+      "rulePriority": 2,
+      "description": "Delete images archived for more than 365 days",
+      "selection": {
+        "tagStatus": "any",
+        "storageClass": "archive",
+        "countType": "sinceImageTransitioned",
+        "countUnit": "days",
+        "countNumber": 365
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
---
+++ terraform/modules/ecr/variables.tf
@@ -0,0 +1,26 @@
+variable "scan_on_push" {
+  description = "Whether to enable image scanning on push"
+  type        = bool
+}
+
+variable "image_tag_mutability" {
+  description = "The tag mutability setting for the repository"
+  type        = string
+}
+
+variable "repository_name" {
+  description = "The name of the ECR repository"
+  type        = string
+}
+
+variable "allow_push_principals" {
+  description = "List of IAM principals allowed to push images to the repository"
+  type        = list(string)
+  default     = []
+}
+
+variable "allow_pull_principals" {
+  description = "List of IAM principals allowed to pull images from the repository"
+  type        = list(string)
+  default     = []
+}
