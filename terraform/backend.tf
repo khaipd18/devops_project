42b628c2691596ae68a205bc28546761ed4b36be
Phan Đình Khải <174919444+khaipd18@users.noreply.github.com>
2026-04-30 20:15:30 +0700

    chore: format backend.tf for improved readability

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "khaipd18-devops-project-tf-state"
-    key = "dev/terraform.tfstate"
-    region = "ap-southeast-1"
-    encrypt = true
+    bucket         = "khaipd18-devops-project-tf-state"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-southeast-1"
+    encrypt        = true
     dynamodb_table = "khaipd18-devops-project-terraform-state-lock"
   }
 }
